ba32fd6b734f3927aeb62f8fac1ab85140629d57
KhadeerBasha1232 <hlobhai041@gmail.com>
2025-05-02 13:26:56 +0530

    whitenoise added

--- backend/urls.py
+++ backend/urls.py
@@ -18,13 +18,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sales/', include('sales.urls')),
     path('auth/', include('authapp.urls')),
-    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
